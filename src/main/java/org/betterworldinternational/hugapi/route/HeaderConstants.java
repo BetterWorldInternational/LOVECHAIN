4d3a6d302852a1f2e9c45dbdb4f1a33986fbc201
Nickolay Laptev <nickolay.laptev@gmail.com>
2017-12-25 22:35:10 +0300

    Added exceptions handlers. Added default headers for api endpoints.

---
+++ src/main/java/org/betterworldinternational/hugapi/route/HeaderConstants.java
@@ -0,0 +1,6 @@
+package org.betterworldinternational.hugapi.route;
+
+final class HeaderConstants {
+    public static final String SERVER_HEADER_NAME = "Server";
+    public static final String SERVER_HEADER_VALUE = "BWI Server";
+}
