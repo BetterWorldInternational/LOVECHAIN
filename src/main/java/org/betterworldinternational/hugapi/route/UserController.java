497e1d65b179a0a77afa3f955c12e384e2f5bd52
Nickolay Laptev <nickolay.laptev@gmail.com>
2017-12-25 19:56:20 +0300

    Moved UserRoute to Spring based controller

--- src/main/java/org/betterworldinternational/hugapi/route/UserController.java
+++ src/main/java/org/betterworldinternational/hugapi/route/UserController.java
@@ -1,10 +1,8 @@
 package org.betterworldinternational.hugapi.route;
 
-import com.google.gson.Gson;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-import org.betterworldinternational.hugapi.exception.HugException;
 import org.betterworldinternational.hugapi.route.request.DidItRequest;
 import org.betterworldinternational.hugapi.route.request.RegisterRequest;
 import org.betterworldinternational.hugapi.route.response.EffectResponse;
@@ -13,49 +11,54 @@ import org.betterworldinternational.hugapi.route.response.MessageResponse;
 import org.betterworldinternational.hugapi.route.response.TokenResponse;
 import org.betterworldinternational.hugapi.service.UserService;
 import org.betterworldinternational.hugapi.validation.UserValidation;
-import spark.Request;
-import spark.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Random;
 
-public class UserRoute {
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     private final UserService userService;
     private final UserValidation userValidation;
-    private final Gson gson;
 
-    public UserRoute(UserService userService, UserValidation userValidation, Gson gson) {
+    @Autowired
+    public UserController(UserService userService, UserValidation userValidation) {
         this.userService = userService;
         this.userValidation = userValidation;
-        this.gson = gson;
     }
 
-    public TokenResponse register(Request request, Response response) {
-        if (!isValidJson(request.body())) {
-            throw new HugException("not valid json request");
-        }
-
-        RegisterRequest registerRequest = gson.fromJson(request.body(), RegisterRequest.class);
+    @RequestMapping(value = "register/", method = RequestMethod.POST)
+    public TokenResponse register(@RequestBody RegisterRequest registerRequest) {
+        logger.info("Register request");
         userValidation.validate(registerRequest);
 
         return userService.registerUser(registerRequest);
     }
 
-    public MessageResponse didIt(Request request, Response response) {
-        int userId = userService.getUserIdByToken(request.headers("token"));
+    @RequestMapping(value = "didit/", method = RequestMethod.POST)
+    public MessageResponse didIt(
+            @RequestHeader("token") String token,
+            @RequestBody DidItRequest didItRequest,
+            @RequestParam(value = "image", required = false) String image) {
+        logger.info("DidIt request");
 
-        if (!isValidJson(request.body())) {
-            throw new HugException("not valid json request");
-        }
-
-        DidItRequest didItRequest = gson.fromJson(request.body(), DidItRequest.class);
+        int userId = userService.getUserIdByToken(token);
 
         if (didItRequest.getLatitude() == 0 || didItRequest.getLongitude() == 0) {
-
             float random = new Random().nextFloat() * (0.9f - 0.09f) + 0.09f;
 
             try {
-
                 HttpResponse<JsonNode> res = Unirest.get("http://freegeoip.net/json/{ip}")
                         .routeParam("ip", "188.247.75.179")
                         .asJson();
@@ -72,12 +75,14 @@ public class UserRoute {
             }
         }
 
-        userService.userDidIt(userId, request.queryParams("image"), didItRequest);
+        userService.userDidIt(userId, image, didItRequest);
 
         return new MessageResponse(true, "success");
     }
 
-    public List<MapPin> map(Request request, Response response) {
+    @RequestMapping(value = "map/", method = RequestMethod.GET)
+    public List<MapPin> map() {
+        logger.info("Map request");
         List<MapPin> mapPins = userService.getMap();
         for (MapPin pin : mapPins) {
             if (pin.getImage() == null) {
@@ -89,16 +94,10 @@ public class UserRoute {
         return mapPins;
     }
 
-    public EffectResponse effect(Request request, Response response) {
-        int userId = userService.getUserIdByToken(request.headers("token"));
+    @RequestMapping(value = "effect/", method = RequestMethod.GET)
+    public EffectResponse effect(@RequestHeader("token") String token) {
+        logger.info("Effect request");
+        int userId = userService.getUserIdByToken(token);
         return userService.getEffect(userId);
-    }
-
-    private boolean isValidJson(String json) {
-        try {
-            return gson.fromJson(json, Object.class) != null;
-        } catch (Exception ex) {
-            return false;
-        }
     }
 }
