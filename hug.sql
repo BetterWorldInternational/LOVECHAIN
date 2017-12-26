9b800f6c8ca28935e7069a0dd3c9c6d1c42cd9b6
Nickolay Laptev <nickolay.laptev@gmail.com>
2017-12-26 11:19:55 +0300

    Updated DB schema

--- hug.sql
+++ hug.sql
@@ -5,16 +5,11 @@
 -- HeidiSQL Version:             9.3.0.4984
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 CREATE DATABASE IF NOT EXISTS hug CHARACTER SET utf8mb4;
 USE hug;
 
--- Dumping structure for table hug.users
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL DEFAULT '',
   `email` varchar(255) NOT NULL DEFAULT '',
@@ -26,13 +21,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `latitude` double DEFAULT NULL,
   `longitude` double DEFAULT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `image` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `challenges_fk` (`challengerId`),
   CONSTRAINT `challenges_fk` FOREIGN KEY (`challengerId`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8;
 
--- Dumping structure for table hug.invites
-CREATE TABLE IF NOT EXISTS `invites` (
+DROP TABLE IF EXISTS `invites`;
+CREATE TABLE `invites` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `phone` varchar(255) NOT NULL DEFAULT '',
   `inviter` int(11) NOT NULL,
@@ -42,7 +38,43 @@ CREATE TABLE IF NOT EXISTS `invites` (
   CONSTRAINT `inviter_fk` FOREIGN KEY (`inviter`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table hug.invites: ~0 rows (approximately)
-DELETE FROM `invites`;
-/*!40000 ALTER TABLE `invites` DISABLE KEYS */;
-/*!40000 ALTER TABLE `invites` ENABLE KEYS */;
+DROP PROCEDURE IF EXISTS recursive_lookup;
+
+DELIMITER //
+
+CREATE PROCEDURE `recursive_lookup`(IN `root_id` INT, IN `isEffect` TINYINT)
+BEGIN
+  -- declare a variable for breaking the while loop
+  DECLARE `done` TINYINT DEFAULT 0;
+
+  -- table to hold challengerId and insert first id from parameter
+  DROP TABLE IF EXISTS `challengers`;
+  CREATE TEMPORARY TABLE `challengers`(`id` INT, UNIQUE KEY (`id`));
+  INSERT INTO `challengers`(`id`) VALUES (`root_id`);
+
+  -- table to hold results
+  DROP TABLE IF EXISTS `results`;
+  CREATE TEMPORARY TABLE `results`(`id` INT, `completed` TINYINT, UNIQUE KEY (`id`));
+  INSERT INTO `results`(`id`, `completed`) SELECT id, completed FROM `users` where id = `root_id`;
+
+  WHILE `done` = 0 DO
+    -- insert new ids from previous loop (or none)
+    INSERT IGNORE INTO `challengers`(`id`) SELECT `id` FROM `results`;
+
+    -- insert new children
+    INSERT IGNORE INTO `results`(`id`, `completed`) SELECT u.id, u.completed FROM `users` u JOIN `challengers` c ON u.challengerId = c.id;
+
+    -- if no rows were inserted, break the loop
+    -- rows already present in the table are not counted
+    IF ROW_COUNT() = 0 THEN
+      SET `done` = 1;
+    END IF;
+  END WHILE;
+  IF `isEffect` = 0 THEN
+        SELECT COUNT(*) total FROM `results`;
+  ELSE
+    SELECT COUNT(*) total FROM `results` WHERE `completed` = 1;
+  END IF;
+END;//
+
+DELIMITER ;
